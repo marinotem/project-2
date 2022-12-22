3c6259c20a8912bdcb420a559a6975919f2381d3
Taylor Marino <55257639+marinotem@users.noreply.github.com>
2022-12-21 22:46:02 -0500

    Updating with changes to code

---
+++ create_table_sql.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE homicide_weather (
+	City VARCHAR(30),
+	Year INT,
+	Month VARCHAR(30),
+	Record_ID INT,
+	Humidity FLOAT, 
+	Most_Common_Weather_Event VARCHAR(30),
+	Temperature FLOAT
+	);
